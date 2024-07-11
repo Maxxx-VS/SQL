cddf834a62c9c0910949b22fed63d8d19cd26c96
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-07-11 21:14:52 +0600

    SQL + таблтица

--- sql_lite_study.py
+++ sql_lite_study.py
@@ -1,18 +1,28 @@
 import sqlite3 as sq
 
-with sq.connect("saper.db") as con:
+with sq.connect("cars.db") as con:
     cur = con.cursor()
 
     # cur.execute("DROP TABLE IF EXISTS users")
 
-    cur.execute("SELECT * FROM users WHERE score > 100 ORDER BY score DESC LIMIT 5")
+    cur.execute("""CREATE TABLE IF NOT EXISTS cars (
+        car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        model TEXT,
+        price INTEGER)
+    """)
+
+    cur.execute("INSERT INTO cars VALUES(1, 'Audi', 52642)")
+    cur.execute("INSERT INTO cars VALUES(2, 'Mercedes', 57246)")
+    cur.execute("INSERT INTO cars VALUES(3, 'Skoda', 9000)")
+    cur.execute("INSERT INTO cars VALUES(4, 'Volvo', 29000)")
+    cur.execute("INSERT INTO cars VALUES(5, 'Bentlt', 350000)")
 
 
     # for result in cur:
     #     print(result)
 
-    result_1 = cur.fetchone()
-    print(result_1)
-
-    result_2 = cur.fetchmany(2)
-    print(result_2)
+    # result_1 = cur.fetchone()
+    # print(result_1)
+    #
+    # result_2 = cur.fetchmany(2)
+    # print(result_2)
