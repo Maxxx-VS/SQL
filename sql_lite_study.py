37d887e838f6332560b170713746aab6df0a9c6c
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-07-11 21:45:52 +0600

    SQL - методы

--- sql_lite_study.py
+++ sql_lite_study.py
@@ -14,20 +14,28 @@ try:
     cur = con.cursor()
 
     cur.execute("""CREATE TABLE IF NOT EXISTS cars (
-        car_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        model TEXT,
-        price INTEGER)
-    """)
-
-    cur.executescript("""
-        DELETE FROM cars WHERE model LIKE 'A%';
+            car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            model TEXT,
+            price INTEGER
+        );
+        BEGIN;
+        INSERT INTO cars VALUES(NULL, 'Audi', 52642);
+        INSERT INTO cars VALUES(NULL, 'Mercedes', 57246);
+        INSERT INTO cars VALUES(NULL, 'Skoda', 9000);
+        INSERT INTO cars VALUES(NULL, 'Volvo', 29000);
+        INSERT INTO cars VALUES(NULL, 'Bentlt', 350000);
         UPDATE cars SET price = price+1000
     """)
+
+    # cur.executescript("""
+    #     DELETE FROM cars WHERE model LIKE 'A%';
+    #     UPDATE cars SET price = price+1000
+    # """)
 
     con.commit() # сохраняет в БД все изменения
 
 except sq.Error as e:
-    if con: con.rollback()
+    if con: con.rollback() # откатывает состояние БД в исходное
     print("Ошибка выполнения запроса")
 finally:
     if con: con.close()
